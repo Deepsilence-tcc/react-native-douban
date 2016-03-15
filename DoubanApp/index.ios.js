bb0aa411b3a9747897d01d755ac32fd3f832bc82
edagarli <lizhi@lydiabox.com>
2016-03-15 22:24:15 +0800

    update

---
+++ DoubanApp/index.ios.js
@@ -0,0 +1,91 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+ var React = require('react-native');
+ var Navigation = require('./ios_views/common/navigation');
+ var Book = require('./ios_views/book/book_list');
+ var Music = require('./ios_views/music/music');
+ var Movie = require('./ios_views/movie/movie');
+
+ var {
+   AppRegistry,
+   StyleSheet,
+   Text,
+   View,
+   TabBarIOS,
+   ScrollView,
+   StatusBarIOS
+ } = React;
+
+ StatusBarIOS.setHidden(true);
+ var DoubanApp = React.createClass({
+   getInitialState: function(){
+     return {
+       selectedTab: '图书'
+     };
+   },
+   render: function() {
+     return (
+       <TabBarIOS>
+         <TabBarIOS.Item
+           title="图书"
+           selected={this.state.selectedTab === '图书'}
+           icon={require('image!book')}
+           onPress={() => {
+             this.setState({
+               selectedTab: '图书'
+             });
+           }}>
+            <Navigation component={Book}/>
+         </TabBarIOS.Item>
+
+         <TabBarIOS.Item
+           title="电影"
+           selected={this.state.selectedTab === '电影'}
+           icon={require('image!movie')}
+           onPress={() => {
+             this.setState({
+               selectedTab: '电影'
+             });
+           }}>
+            <Navigation component={Movie}/>
+         </TabBarIOS.Item>
+
+         <TabBarIOS.Item
+           title="音乐"
+           selected={this.state.selectedTab === '音乐'}
+           icon={require('image!music')}
+           onPress={() => {
+             this.setState({
+               selectedTab: '音乐'
+             });
+           }}>
+           <Navigation component={Music}/>
+         </TabBarIOS.Item>
+       </TabBarIOS>
+     );
+   }
+ });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('DoubanApp', () => DoubanApp);
---
+++ DoubanApp/ios_views/music/music.js
@@ -0,0 +1,20 @@
+var React = require('react-native');
+
+var {
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+  Image,
+  ScrollView,
+  ActivityIndicatorIOS,
+  TouchableOpacity
+  } = React;
+
+  module.exports = React.createClass({
+      render: function(){
+        return(
+          <View></View>
+        );
+      }
+  });
